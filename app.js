a37d0169333f673208d4bb5d7e0bd754dc3110c7
candearismendi <candearis25@gmail.com>
2023-11-13 12:15:10 -0300

    proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const port = 3000
+
+let pathPublic = path.resolve("public")
+
+console.log(pathPublic);
+
+app.use(express.static(pathPublic))
+
+app.listen(port,()=>{
+    console.log("El servidor esta funcionando");
+})
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
+})
+
+const pathRegister = path.resolve(__dirname, "views/register.html");
+app.get("/register", (req, res) => {
+    res.sendFile(pathRegister);
+});
+
+const pathLogin = path.resolve(__dirname, "views/login.html");
+app.get("/login", (req, res) => {
+    res.sendFile(pathLogin);
+});
